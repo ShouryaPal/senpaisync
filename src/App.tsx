d8b62f8168868392ecbfbdebb4e84b3b5b88309f
Shourya Pal <palshourya897@gmail.com>
2025-01-13 22:07:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl">SenpaiSync</h1>
+      <Button className="bg-black">Hello</Button>
+    </>
+  );
+}
+
+export default App;
